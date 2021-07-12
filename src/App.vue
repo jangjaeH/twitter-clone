c73784adb45d44f5dabea3693c849d3269f08b24
jangjaeH <pvcgrdo@gmail.com>
2021-07-12 23:55:06 +0900

    :rocket: feat:side  layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,62 @@
 <template>
-    <h1 class="text-lg bg-gray-900">hello world</h1>
+    <div class="flex h-screen">
+        <div class="w-20 xl:w-1/4 pt-5 xl:ml-10 flex flex-col justify-between border-r border-gray-100">
+            <div class="flex flex-col items-start">
+                <!-- twitter logo -->
+                <i class="fab fa-twitter text-3xl text-primary xl:ml-4 mb-3"></i>
+                <!-- sidemenu icons -->
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="fas fa-home fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">홈</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="fas fa-hashtag fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">탐색하기</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="far fa-bell fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">알림</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="far fa-envelope fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">쪽지</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="far fa-bookmark fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">북마크</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="far fa-list-alt fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">리스트</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="far fa-user fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">프로필</span>
+                </div>
+                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                    <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+                    <span class="ml-5 text-lg">더보기</span>
+                </div>
+            </div>
+            <!-- tweet button -->
+            <div class="mr-3">
+                <button class="mt-3 bg-primary text-white w-full h-12 rounded-full hover:bg-dark foucs:outline-none">
+                    트윗
+                </button>
+            </div>
+            <div class="mr-3 mb-3">
+                <button class="mt-3 px-2 py-1 w-full h-12 rounded full hover:bg-blue-50 flex items-center">
+                    <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full" />
+                    <div class="ml-2">
+                        <div class="text-sm font-bold">jhjang.com</div>
+                        <div class="text-xs text-gray-500 text-left">@jhjang</div>
+                    </div>
+                    <i class="ml-auto fas fa-ellipsis-h fa-fw text-x5"></i>
+                </button>
+            </div>
+        </div>
+        <div class="xl:pr-3 mb-3 relative"></div>
+    </div>
 </template>
 <script>
 export default {};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#1DA1F2',
+                dark: '#1a91da',
+                light: '#a5d0f5',
+                lighter: '#D4E5F4',
+            },
+        },
     },
     variants: {
         extend: {},
