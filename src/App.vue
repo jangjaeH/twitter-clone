003b8733e78732a735617cd2c9a59a3e8df6748b
jangjaeH <pvcgrdo@gmail.com>
2021-07-16 01:04:01 +0900

    :rocket:feat: notification

--- src/App.vue
+++ src/App.vue
@@ -67,170 +67,22 @@
                 </div>
             </div>
         </div>
-        <div class="flex-1">
-            <div class="flex h-screen">
-                <!-- main section -->
-                <div class="flex-1 border-r border-gray-100 overflow-y-auto">
-                    <div class="flex flex-col">
-                        <!-- page title -->
-                        <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">홈</div>
-                        <!-- tweeting setion -->
-                        <div class="flex px-3 py-3 border-b-8 border-gray-100">
-                            <img
-                                src="http://picsum.photos/200"
-                                class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"
-                            />
-                            <div class="ml-2 flex-1 flex flex-col">
-                                <textarea
-                                    id="textarea"
-                                    placeholder="무슨 일이 일어나고 있나요?"
-                                    class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"
-                                ></textarea>
-                                <div class="text-right">
-                                    <button
-                                        class="
-                                            bg-primary
-                                            hover:bg-dark
-                                            text-sm
-                                            font-bold
-                                            text-white
-                                            px-4
-                                            py-1
-                                            rounded-full
-                                        "
-                                    >
-                                        트윗
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- tweets -->
-                        <div class="flex px-3 py-3 border-b border-gray-100" v-for="tweet in 10" :key="tweet">
-                            <img
-                                src="http://picsum.photos/200"
-                                class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"
-                            />
-                            <div class="ml-3 flex-1 flex flex-col space-y-1">
-                                <div class="text-sm space-x-1">
-                                    <span class="font-bold">KMS(T) 패치 알리미</span>
-                                    <span class="text-gray-500 text-xs">@kmspatcher</span>
-                                    <span>·</span>
-                                    <span class="text-graqy-500 text-xs">2019년 7월 17일</span>
-                                </div>
-                                <!-- tweet body -->
-                                <div>
-                                    http://inven.co.kr/board/maple/2304/16586 안녕하세요. WzComparerR2-KMS을 수정하여
-                                    캐시 이펙트와 믹스염색이 가능하도록 만들어보았습니다. 해당 링크에서 다운로드 받으실
-                                    수 있습니다. 감사합니다.
-                                </div>
-                                <!-- tweet actions -->
-                                <div class="flex justify-between">
-                                    <div class="text-gray-500 hover:text-primary">
-                                        <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
-                                        <span class="ml-1 textgray-500 text-sm">11</span>
-                                    </div>
-                                    <div class="text-gray-500 hover:text-green-500">
-                                        <i class="fas fa-retweet hover:bg-blue-50 rounded-full p-2"></i>
-                                        <span class="ml-1 textgray-500 text-sm">11</span>
-                                    </div>
-                                    <div class="text-gray-500 hover:text-read-500">
-                                        <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
-                                        <span class="ml-1 textgray-500 text-sm">11</span>
-                                    </div>
-                                    <div class="text-gray-500 hover:text-primary">
-                                        <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- trend section -->
-                <div class="hidden lg:block lg:w-2/5 py-2">
-                    <!-- tweet search input -->
-                    <div class="relative mb-5">
-                        <input
-                            placeholder="트위터 검색"
-                            type="text"
-                            class="
-                                pl-10
-                                rounded-full
-                                w-full
-                                p-2
-                                bg-gray-100
-                                text-sm
-                                focus:right-1 focus:ring-primary focus:bg-white focus:outline-none
-                            "
-                        />
-                        <i class="fas fa-search absolute left-0 mt-3 ml-3 text-sm text-light" />
-                    </div>
-                    <div class="w-full rounded-lg bg-gray-50 flex flex-col">
-                        <div class="flex items-center justify-between p-3 py-2 border-b border-gray-100">
-                            <span class="text-lg font-bold">나를 위한 트렌드</span>
-                            <i
-                                class="text-cneter fas fa-cog text-lg text-primary hover:bg-blue-50 p-2 rounded-full"
-                            ></i>
-                        </div>
-                        <div
-                            class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
-                            v-for="trend in 3"
-                            :key="trend"
-                        >
-                            <div class="flex">
-                                <div class="flex-1 flex flex-col">
-                                    <div class="text-gray-500">대한민국에서 트렌드 중</div>
-                                    <div class="font-bold text-lg">시크릿 길드</div>
-                                    <div class="text-gray-500">3,564 트윗</div>
-                                </div>
-                                <i class="fas fa-ellipsis-h text-gray-500"></i>
-                            </div>
-                        </div>
-                        <div class="px-4 py-1 hover:bg-gray-100">
-                            <span class="text-primary text-lg px-4 py-2 rounded-xl">더보기</span>
-                        </div>
-                    </div>
-                    <!-- follow recommends -->
-                    <div class="mt-5 w-full rounded-lg bg-gray-50 flex flex-col">
-                        <div class="p-3 py-3 border-b border-gray-100">
-                            <span class="text-lg font-bold">팔로우 추천</span>
-                        </div>
-                        <div
-                            class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
-                            v-for="trend in 3"
-                            :key="trend"
-                        >
-                            <div class="flex items-center">
-                                <img src="http://picsum.photos/200" class="w-12 h-12 rounded-full mr-2" />
-                                <div class="flex-1 flex flex-col">
-                                    <div class="font-bold text-lg">시크릿</div>
-                                    <div class="text-gray-500">@Screet</div>
-                                </div>
-                                <div>
-                                    <button
-                                        class="
-                                            border-2 border-primary
-                                            hover:bg-blue-100
-                                            px-4
-                                            py-1
-                                            rounded-full
-                                            text-primary
-                                        "
-                                    >
-                                        팔로우
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="px-4 py-2 hover:bg-gray-100">
-                            <span class="text-primary text-lg px-4 py-2 rounded-xl">더보기</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <!-- main section -->
+        <div class="flex-1 flex h-screen">
+            <!-- <Notification /> -->
+            <Home />
+            <Trends />
         </div>
     </div>
 </template>
 <script>
-export default {};
+import Home from './pages/Home.vue';
+import Notification from './pages/Notifications.vue';
+import Trends from './components/trends.vue';
+
+export default {
+    components: { Home, Trends, Notification },
+    setup() {},
+};
 </script>
 <style></style>
---
+++ src/components/trends.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="hidden lg:block lg:w-2/5 py-2">
+        <!-- tweet search input -->
+        <div class="relative mb-5">
+            <input
+                placeholder="트위터 검색"
+                type="text"
+                class="
+                    pl-10
+                    rounded-full
+                    w-full
+                    p-2
+                    bg-gray-100
+                    text-sm
+                    focus:right-1 focus:ring-primary focus:bg-white focus:outline-none
+                "
+            />
+            <i class="fas fa-search absolute left-0 mt-3 ml-3 text-sm text-light" />
+        </div>
+        <div class="w-full rounded-lg bg-gray-50 flex flex-col">
+            <div class="flex items-center justify-between p-3 py-2 border-b border-gray-100">
+                <span class="text-lg font-bold">나를 위한 트렌드</span>
+                <i class="text-cneter fas fa-cog text-lg text-primary hover:bg-blue-50 p-2 rounded-full"></i>
+            </div>
+            <div
+                class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
+                v-for="trend in 3"
+                :key="trend"
+            >
+                <div class="flex">
+                    <div class="flex-1 flex flex-col">
+                        <div class="text-gray-500">대한민국에서 트렌드 중</div>
+                        <div class="font-bold text-lg">시크릿 길드</div>
+                        <div class="text-gray-500">3,564 트윗</div>
+                    </div>
+                    <i class="fas fa-ellipsis-h text-gray-500"></i>
+                </div>
+            </div>
+            <div class="px-4 py-1 hover:bg-gray-100">
+                <span class="text-primary text-lg px-4 py-2 rounded-xl">더보기</span>
+            </div>
+        </div>
+        <!-- follow recommends -->
+        <div class="mt-5 w-full rounded-lg bg-gray-50 flex flex-col">
+            <div class="p-3 py-3 border-b border-gray-100">
+                <span class="text-lg font-bold">팔로우 추천</span>
+            </div>
+            <div
+                class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
+                v-for="trend in 3"
+                :key="trend"
+            >
+                <div class="flex items-center">
+                    <img src="http://picsum.photos/200" class="w-12 h-12 rounded-full mr-2" />
+                    <div class="flex-1 flex flex-col">
+                        <div class="font-bold text-lg">시크릿</div>
+                        <div class="text-gray-500 text-sm">@Screet</div>
+                    </div>
+                    <div>
+                        <button
+                            class="
+                                border-2 border-primary
+                                hover:bg-blue-100
+                                px-2
+                                py-1
+                                rounded-full
+                                text-primary text-sm
+                            "
+                        >
+                            팔로우
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="px-4 py-2 hover:bg-gray-100">
+                <span class="text-primary text-lg px-4 py-2 rounded-xl">더보기</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style></style>
