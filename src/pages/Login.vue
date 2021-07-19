881bf092a3a8c4f8269807d29a97ae10862951bb
jangjaeH <pvcgrdo@gmail.com>
2021-07-20 00:51:11 +0900

    :fire:feat:login & register

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>login view</div>
+    <div class="flex flex-col items-center space-y-4 mt-10">
+        <i :class="`fab fa-twitter text-4xl text-primary ${loading ? 'animate-bounce' : ''}`"></i>
+        <span class="text-2xl font-bold">트위터 로그인</span>
+        <input
+            v-model="email"
+            type="text"
+            class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
+            placeholder="이메일"
+        />
+        <input
+            v-model="password"
+            type="text"
+            class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
+            placeholder="비밀번호"
+        />
+        <button class="w-96 rounded-full bg-primary text-white py-3 hover:bg-dark" @click="onLogin">로그인</button>
+        <router-link to="/Register">
+            <button class="text-primary">계정이 없으신가요? 회원가입 하기</button>
+        </router-link>
+    </div>
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+export default {
+    setup() {
+        const username = ref('');
+        const email = ref('');
+        const password = ref('');
+        const loading = ref(false);
+        const onLogin = () => {
+            console.log(username.value, email.value, password.value);
+        };
+
+        return {
+            username,
+            email,
+            password,
+            loading,
+            onLogin,
+        };
+    },
+};
 </script>
 
 <style></style>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,59 +1,52 @@
 <template>
-  <div class="flex flex-col items-center space-y-4 mt-10">
-    <i
-      :class="`fab fa-twitter text-4xl text-primary ${
-        loading ? 'animate-bounce' : ''
-      }`"
-    ></i>
-    <span class="text-2xl font-bold">트위터 회원가입</span>
-    <input
-      v-model="email"
-      type="text"
-      class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
-      placeholder="이메일"
-    />
-    <input
-      v-model="username"
-      type="text"
-      class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
-      placeholder="아이디"
-    />
-    <input
-      v-model="password"
-      type="text"
-      class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
-      placeholder="비밀번호"
-    />
-    <button
-      class="w-96 rounded-full bg-primary text-white py-3 hover:bg-dark"
-      @click="onRegister"
-    >
-      회원가입
-    </button>
-    <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
-  </div>
+    <div class="flex flex-col items-center space-y-4 mt-10">
+        <i :class="`fab fa-twitter text-4xl text-primary ${loading ? 'animate-bounce' : ''}`"></i>
+        <span class="text-2xl font-bold">트위터 회원가입</span>
+        <input
+            v-model="email"
+            type="text"
+            class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
+            placeholder="이메일"
+        />
+        <input
+            v-model="username"
+            type="text"
+            class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
+            placeholder="아이디"
+        />
+        <input
+            v-model="password"
+            type="text"
+            class="rounded w-96 px-4 py-3 border border-gray-300 focus:outline-none focus:ring-2 focus:border-primary"
+            placeholder="비밀번호"
+        />
+        <button class="w-96 rounded-full bg-primary text-white py-3 hover:bg-dark" @click="onRegister">회원가입</button>
+        <router-link to="/Login">
+            <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
+        </router-link>
+    </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref } from 'vue';
 export default {
-  setup() {
-    const username = ref("");
-    const email = ref("");
-    const password = ref("");
-    const loading = ref(false);
-    const onRegister = () => {
-      console.log(username.value, email.value, password.value);
-    };
+    setup() {
+        const username = ref('');
+        const email = ref('');
+        const password = ref('');
+        const loading = ref(false);
+        const onRegister = () => {
+            console.log(username.value, email.value, password.value);
+        };
 
-    return {
-      username,
-      email,
-      password,
-      loading,
-      onRegister,
-    };
-  },
+        return {
+            username,
+            email,
+            password,
+            loading,
+            onRegister,
+        };
+    },
 };
 </script>
 
