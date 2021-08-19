e0ea0eb6ab988800aae6e0f9f5f36b7e04e77036
jangjaeH <pvcgrdo@gmail.com>
2021-08-19 23:44:48 +0900

    :hammer:fix:notifications created_at => create_at

--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex-1 border-gray-100 flex flex-col overflow-y-auto">
-        <!-- Title -->
+    <!-- notification -->
+    <div class="flex-1 border-r border-gray-100 flex flex-col overflow-y-auto">
+        <!-- title -->
         <div class="p-3 font-bold text-lg border-b border-gray-100">알림</div>
         <!-- notifications -->
         <div
@@ -29,17 +30,14 @@
                 ></i>
             </div>
             <div>
-                <span class="font-bold">{{ notification.username }}</span>
+                <span class="font-bold"> {{ notification.username }}</span> 님의 최근 트윗
             </div>
-            <router-link :to="`/tweet/${notification.id}`">
-                <div class="text-gray-500">
-                    {{ notification.tweetBody }}
-                </div>
-            </router-link>
+            <router-link :to="`/tweet/${notification.id}`" class="text-gray-500">{{
+                notification.tweet_body
+            }}</router-link>
         </div>
     </div>
 </template>
-
 <script>
 import { ref, computed, onBeforeMount } from 'vue';
 import store from '../store';
@@ -50,11 +48,12 @@ export default {
         const currentUser = computed(() => store.state.user);
         const notifications = ref([]);
         onBeforeMount(() => {
+            console.log(currentUser.value.followings);
             currentUser.value.followings.forEach(async (following) => {
                 const dateFrom = Date.now() - 60 * 60 * 24 * 7 * 1000; // before 7 days
-                const snapshot = await TWEET_COLEECTION.where('created_at', '>', dateFrom)
+                const snapshot = await TWEET_COLEECTION.where('create_at', '>', dateFrom)
                     .where('uid', '==', following)
-                    .orderBy('created_at', 'desc')
+                    .orderBy('create_at', 'desc')
                     .get();
                 snapshot.docs.forEach(async (doc) => {
                     let tweet = await getTweetInfo(doc.data(), currentUser.value);
