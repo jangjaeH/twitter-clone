d6dc21561851a23be517ef709480a475c338a410
jangjaeH <38302881+jangjaeH@users.noreply.github.com>
2021-11-20 17:34:10 +0900

    :rocket:update:Recommend follow

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,98 +1,150 @@
 <template>
-    <!-- tweets -->
-    <div class="flex px-3 py-3 border-b border-gray-100 hover:bg-gray-50 cursor-pointer">
-        <router-link :to="`/profile/${tweet.uid}`">
-            <img :src="tweet.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
-        </router-link>
-        <div class="ml-3 flex-1 flex flex-col space-y-1">
-            <div class="text-sm flex justify-between items-center">
-                <div class="space-x-2">
-                    <span class="font-bold">{{ tweet.email }}</span>
-                    <span class="text-gray-500 text-xs">@{{ tweet.username }}</span>
-                    <span>·</span>
-                    <span class="text-graqy-500 text-xs">{{ moment(tweet.create_at).fromNow() }}</span>
-                </div>
-                <button v-if="currentUser.uid === tweet.uid" @click="onDeleteTweet(tweet)">
-                    <i class="fas fa-trash text-red-400 p-2 rounded-full hover:bg-red-50"></i>
-                </button>
-            </div>
-            <!-- tweet body -->
-            <div>
-                <router-link :to="`/tweet/${tweet.id}`">{{ tweet.tweet_body }}</router-link>
-            </div>
-            <!-- tweet actions -->
-            <div class="flex justify-between">
-                <div @click="showCommentModal = true" class="text-gray-500 hover:text-primary">
-                    <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
-                    <span class="ml-1 textgray-500 text-sm">{{ tweet.num_comments }}</span>
-                </div>
-                <div v-if="!tweet.isRetweeted" class="text-gray-500 hover:text-green-400" @click="handleRetweet(tweet)">
-                    <i class="fas fa-retweet hover:bg-blue-50 rounded-full p-2"></i>
-                    <span class="ml-1 textgray-500 text-sm">{{ tweet.num_retweets }}</span>
-                </div>
-                <div v-else class="text-green-400" @click="handleRetweet(tweet)">
-                    <i class="fas fa-retweet hover:bg-blue-50 rounded-full p-2"></i>
-                    <span class="ml-1 textgray-500 text-sm">{{ tweet.num_retweets }}</span>
-                </div>
-                <div v-if="!tweet.isLiked" class="text-gray-400 hover:text-read-400" @click="handleLikes(tweet)">
-                    <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
-                    <span class="ml-1 textgray-500 text-sm">{{ tweet.num_likes }}</span>
-                </div>
-                <div v-else class="text-red-400" @click="handleLikes(tweet)">
-                    <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
-                    <span class="ml-1 textgray-500 text-sm">{{ tweet.num_likes }}</span>
-                </div>
-                <div class="text-gray-500 hover:text-primary">
-                    <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
-                </div>
-            </div>
+  <!-- tweets -->
+  <div
+    class="flex px-3 py-3 border-b border-gray-100 hover:bg-gray-50 cursor-pointer"
+  >
+    <router-link :to="`/profile/${tweet.uid}`">
+      <img
+        :src="tweet.profile_image_url"
+        class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"
+      />
+    </router-link>
+    <div class="ml-3 flex-1 flex flex-col space-y-1">
+      <div class="text-sm flex justify-between items-center">
+        <div class="space-x-2">
+          <span class="font-bold">{{ tweet.email }}</span>
+          <span class="text-gray-500 text-xs">@{{ tweet.username }}</span>
+          <span>·</span>
+          <span class="text-graqy-500 text-xs">{{
+            moment(tweet.create_at).fromNow()
+          }}</span>
         </div>
-        <comment-modal :tweet="tweet" v-if="showCommentModal" @close-modal="showCommentModal = false"></comment-modal>
+        <button
+          v-if="currentUser.uid === tweet.uid"
+          @click="onDeleteTweet(tweet)"
+        >
+          <i
+            class="fas fa-trash text-red-400 p-2 rounded-full hover:bg-red-50"
+          ></i>
+        </button>
+      </div>
+      <!-- tweet body -->
+      <div>
+        <router-link :to="`/tweet/${tweet.id}`">{{
+          tweet.tweet_body
+        }}</router-link>
+      </div>
+      <!-- tweet actions -->
+      <div class="flex justify-between">
+        <div
+          @click="showCommentModal = true"
+          class="text-gray-500 hover:text-primary"
+        >
+          <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
+          <span class="ml-1 textgray-500 text-sm">{{
+            tweet.num_comments
+          }}</span>
+        </div>
+        <div
+          v-if="!tweet.isRetweeted"
+          class="text-gray-500 hover:text-green-400"
+          @click="handleRetweet(tweet)"
+        >
+          <i class="fas fa-retweet hover:bg-blue-50 rounded-full p-2"></i>
+          <span class="ml-1 textgray-500 text-sm">{{
+            tweet.num_retweets
+          }}</span>
+        </div>
+        <div v-else class="text-green-400" @click="handleRetweet(tweet)">
+          <i class="fas fa-retweet hover:bg-blue-50 rounded-full p-2"></i>
+          <span class="ml-1 textgray-500 text-sm">{{
+            tweet.num_retweets
+          }}</span>
+        </div>
+        <div
+          v-if="!tweet.isLiked"
+          class="text-gray-400 hover:text-read-400"
+          @click="handleLikes(tweet)"
+        >
+          <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
+          <span class="ml-1 textgray-500 text-sm">{{ tweet.num_likes }}</span>
+        </div>
+        <div v-else class="text-red-400" @click="handleLikes(tweet)">
+          <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
+          <span class="ml-1 textgray-500 text-sm">{{ tweet.num_likes }}</span>
+        </div>
+        <div class="text-gray-500 hover:text-primary">
+          <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
+        </div>
+      </div>
     </div>
+    <comment-modal
+      :tweet="tweet"
+      v-if="showCommentModal"
+      @close-modal="showCommentModal = false"
+    ></comment-modal>
+  </div>
 </template>
 
 <script>
-import moment from 'moment';
-import { ref } from 'vue';
-import commentModal from './CommentModal.vue';
-import handleRetweet from '../utils/handleRetweet';
-import handleLikes from '../utils/handleLikes';
-import firebase from 'firebase';
+import moment from "moment";
+import { ref } from "vue";
+import commentModal from "./CommentModal.vue";
+import handleRetweet from "../utils/handleRetweet";
+import handleLikes from "../utils/handleLikes";
+import firebase from "firebase";
 import {
-    COMMENT_COLLECTION,
-    LIKE_COLLECTION,
-    RETWEET_COLLECTION,
-    TWEET_COLEECTION,
-    USER_COLEECTION,
-} from '../firebase';
+  COMMENT_COLLECTION,
+  LIKE_COLLECTION,
+  RETWEET_COLLECTION,
+  TWEET_COLEECTION,
+  USER_COLEECTION,
+} from "../firebase";
 export default {
-    components: { commentModal },
-    props: ['currentUser', 'tweet'],
-    setup() {
-        const showCommentModal = ref(false);
+  components: { commentModal },
+  props: ["currentUser", "tweet"],
+  setup() {
+    const showCommentModal = ref(false);
+    const onDeleteTweet = async (tweet) => {
+      if (confirm("정말로 트윗을 삭제하시겠습니까?")) {
+        // delete tweet
+        await TWEET_COLEECTION.doc(tweet.id).delete();
+        //delete comments
+        const commentSnapshot = await COMMENT_COLLECTION.where(
+          "from_tweet_id",
+          "==",
+          tweet.id
+        ).get();
+        commentSnapshot.docs.forEach(async (doc) => await doc.ref.delete());
+        // delete likes
+        const liekSnapshot = await LIKE_COLLECTION.where(
+          "from_tweet_id",
+          "==",
+          tweet.id
+        ).get();
+        liekSnapshot.docs.forEach(async (doc) => await doc.ref.delete());
+        // delete retweets
+        const retweetCollection = await RETWEET_COLLECTION.where(
+          "from_tweet_id",
+          "==",
+          tweet.id
+        ).get();
+        retweetCollection.docs.forEach(async (doc) => await doc.ref.delete());
+        // user collection - num_tweets (-1)
+        await USER_COLEECTION.doc(tweet.uid).update({
+          num_tweets: firebase.firestore.FieldValue.increment(-1),
+        });
+      }
+    };
 
-        const onDeleteTweet = async (tweet) => {
-            if (confirm('정말로 트윗을 삭제하시겠습니까?')) {
-                // delete tweet
-                await TWEET_COLEECTION.doc(tweet.id).delete();
-                //delete comments
-                const commentSnapshot = await COMMENT_COLLECTION.where('from_tweet_id', '==', tweet.id).get();
-                commentSnapshot.docs.forEach(async (doc) => await doc.ref.delete());
-                // delete likes
-                const liekSnapshot = await LIKE_COLLECTION.where('from_tweet_id', '==', tweet.id).get();
-                liekSnapshot.docs.forEach(async (doc) => await doc.ref.delete());
-                // delete retweets
-                const retweetCollection = await RETWEET_COLLECTION.where('from_tweet_id', '==', tweet.id).get();
-                retweetCollection.docs.forEach(async (doc) => await doc.ref.delete());
-                // user collection - num_tweets (-1)
-                await USER_COLEECTION.doc(tweet.uid).update({
-                    num_tweets: firebase.firestore.FieldValue.increment(-1),
-                });
-            }
-        };
-
-        return { moment, showCommentModal, handleRetweet, handleLikes, onDeleteTweet };
-    },
+    return {
+      moment,
+      showCommentModal,
+      handleRetweet,
+      handleLikes,
+      onDeleteTweet,
+    };
+  },
 };
 </script>
 
--- src/components/trends.vue
+++ src/components/trends.vue
@@ -1,85 +1,169 @@
 <template>
-    <div class="hidden lg:block lg:w-2/5 py-2">
-        <!-- tweet search input -->
-        <div class="relative mb-5">
-            <input
-                placeholder="트위터 검색"
-                type="text"
-                class="
-                    pl-10
-                    rounded-full
-                    w-full
-                    p-2
-                    bg-gray-100
-                    text-sm
-                    focus:right-1 focus:ring-primary focus:bg-white focus:outline-none
-                "
-            />
-            <i class="fas fa-search absolute left-0 mt-3 ml-3 text-sm text-light" />
-        </div>
-        <div class="w-full rounded-lg bg-gray-50 flex flex-col">
-            <div class="flex items-center justify-between p-3 py-2 border-b border-gray-100">
-                <span class="text-lg font-bold">나를 위한 트렌드</span>
-                <i class="text-cneter fas fa-cog text-lg text-primary hover:bg-blue-50 p-2 rounded-full"></i>
-            </div>
-            <div
-                class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
-                v-for="trend in 3"
-                :key="trend"
-            >
-                <div class="flex">
-                    <div class="flex-1 flex flex-col">
-                        <div class="text-gray-500">대한민국에서 트렌드 중</div>
-                        <div class="font-bold text-lg">시크릿 길드</div>
-                        <div class="text-gray-500">3,564 트윗</div>
-                    </div>
-                    <i class="fas fa-ellipsis-h text-gray-500"></i>
-                </div>
-            </div>
-            <div class="px-4 py-1 hover:bg-gray-100">
-                <span class="text-primary text-lg px-4 py-2 rounded-xl">더보기</span>
-            </div>
-        </div>
-        <!-- follow recommends -->
-        <div class="mt-5 w-full rounded-lg bg-gray-50 flex flex-col">
-            <div class="p-3 py-3 border-b border-gray-100">
-                <span class="text-lg font-bold">팔로우 추천</span>
-            </div>
-            <div
-                class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
-                v-for="trend in 3"
-                :key="trend"
-            >
-                <div class="flex items-center">
-                    <img src="http://picsum.photos/200" class="w-12 h-12 rounded-full mr-2" />
-                    <div class="flex-1 flex flex-col">
-                        <div class="font-bold text-lg">시크릿</div>
-                        <div class="text-gray-500 text-sm">@Screet</div>
-                    </div>
-                    <div>
-                        <button
-                            class="
-                                border-2 border-primary
-                                hover:bg-blue-100
-                                px-2
-                                py-1
-                                rounded-full
-                                text-primary text-sm
-                            "
-                        >
-                            팔로우
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="px-4 py-2 hover:bg-gray-100">
-                <span class="text-primary text-lg px-4 py-2 rounded-xl">더보기</span>
-            </div>
-        </div>
+  <div class="hidden lg:block lg:w-2/5 py-2">
+    <!-- tweet search input -->
+    <div class="relative mb-5">
+      <input
+        placeholder="트위터 검색"
+        type="text"
+        class="pl-10 rounded-full w-full p-2 bg-gray-100 text-sm focus:right-1 focus:ring-primary focus:bg-white focus:outline-none"
+      />
+      <i class="fas fa-search absolute left-0 mt-3 ml-3 text-sm text-light" />
     </div>
+    <div class="w-full rounded-lg bg-gray-50 flex flex-col">
+      <div
+        class="flex items-center justify-between p-3 py-2 border-b border-gray-100"
+      >
+        <span class="text-lg font-bold">나를 위한 트렌드</span>
+      </div>
+      <div
+        class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
+        v-for="trend in randomTrends"
+        :key="trend"
+      >
+        <div class="flex">
+          <div class="flex-1 flex flex-col">
+            <router-link :to="`/tweet/${trend.id}`">
+              <div class="text-gray-500 mt-2">#{{ trend.username }}</div>
+              <div class="font-bold text-lg mt-2">{{ trend.tweet_body }}</div>
+              <div class="text-gray-500 mt-2">
+                {{ trend.num_retweets }}명이 트윗
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- follow recommends -->
+    <div class="mt-5 w-full rounded-lg bg-gray-50 flex flex-col">
+      <div class="p-3 py-3 border-b border-gray-100">
+        <span class="text-lg font-bold">팔로우 추천</span>
+      </div>
+      <div
+        class="border-b border-gray-100 hover:bg-gray-200 px-4 py-2 cursor-pointer"
+        v-for="trend in randomRecommendationUser"
+        :key="trend"
+      >
+        <div class="flex items-center">
+          <img
+            :src="trend.profile_image_url"
+            class="w-12 h-12 rounded-full mr-2"
+          />
+          <div class="flex-1 flex flex-col">
+            <div class="font-bold text-lg">{{ trend.email }}</div>
+            <div class="text-gray-500 text-sm">{{ trend.username }}</div>
+          </div>
+          <div
+            v-if="currentUser.followings.includes(trend.uid)"
+            @click="onUnFollow(trend.uid)"
+          >
+            <button
+              class="bg-red-400 border-2 hover:opacity-100 px-2 py-1 rounded-full text-white text-sm"
+            >
+              언팔로우
+            </button>
+          </div>
+          <div v-else @click="onFollow(trend.uid)">
+            <button
+              class="border-2 border-primary hover:bg-blue-100 px-2 py-1 rounded-full text-primary text-sm"
+            >
+              팔로우
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {};
+import { computed, onBeforeMount, ref } from "vue";
+import store from "../store";
+import { TWEET_COLEECTION, USER_COLEECTION } from "../firebase";
+import getTweetInfo from "../utils/getTweetInfo";
+import firebase from "firebase";
+const shuffle = (array) => {
+  array.sort(() => Math.random() - 0.5);
+};
+
+export default {
+  setup() {
+    const currentUser = computed(() => store.state.user);
+    const trends = ref([]);
+    const recommendationUser = ref([]);
+    const user = ref([]);
+    const randomTrends = computed(() => {
+      //불변 객체
+      const copy = [...trends.value];
+      shuffle(copy);
+      return copy.slice(0, 3);
+    });
+
+    const randomRecommendationUser = computed(() => {
+      const copy = [...user.value];
+      shuffle(copy);
+      return copy.slice(0, 3);
+    });
+
+    onBeforeMount(() => {
+      TWEET_COLEECTION.orderBy("create_at", "desc").onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach(async (change) => {
+          let trend = await getTweetInfo(change.doc.data(0), currentUser.value);
+          if (change.type === "added") {
+            trends.value.splice(change.newIndex, 0, trend);
+          } else if (change.type == "modified") {
+            trends.value.splice(change.oldIndex, 1, trend);
+          } else if (change.type == "removed") {
+            trends.value.splice(change.oldIndex, 1);
+          }
+        });
+      });
+
+      USER_COLEECTION.where("uid", "!=", currentUser.value.uid).onSnapshot(
+        (snapshot) => {
+          snapshot.docChanges().forEach((change) => {
+            user.value.push(change.doc.data());
+          });
+        }
+      );
+    });
+
+    const onFollow = async (userId) => {
+      await USER_COLEECTION.doc(currentUser.value.uid).update({
+        followings: firebase.firestore.FieldValue.arrayUnion(userId),
+      });
+
+      await USER_COLEECTION.doc(userId).update({
+        followers: firebase.firestore.FieldValue.arrayUnion(
+          currentUser.value.uid
+        ),
+      });
+
+      store.commit("SET_FOLLOW", userId);
+    };
+
+    const onUnFollow = async (userId) => {
+      await USER_COLEECTION.doc(currentUser.value.uid).update({
+        followings: firebase.firestore.FieldValue.arrayRemove(userId),
+      });
+
+      await USER_COLEECTION.doc(userId).update({
+        followers: firebase.firestore.FieldValue.arrayRemove(
+          currentUser.value.uid
+        ),
+      });
+      store.commit("SET_UN_FOLLOW", userId);
+    };
+
+    return {
+      trends,
+      randomTrends,
+      currentUser,
+      randomRecommendationUser,
+      recommendationUser,
+      onUnFollow,
+      onFollow,
+    };
+  },
+};
 </script>
 
 <style></style>
